02ee95f4cdfaff4a69195d51822f3b617b9e0808
Mayorrkun <mayokunlaoshelawal@gmail.com>
2026-02-17 15:18:51 +0100

    Completed exercise - Storing User Data

---
+++ StoringUserData/StoringUserData/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Define a variable to hold your own name
+Define a variable to hold your own phone number
+Define a variable to hold your age
+Print variables line by line to the screen
+Extra: define variables using the var keyword
+ */
+
+var name = "Tayo";
+var phone = 2348090987542L;
+var age = 30;
+
+Console.WriteLine("");
+Console.WriteLine("Your name is " + name);
+Console.WriteLine("");
+Console.WriteLine("Your phone number is " + Convert.ToString(phone));
+Console.WriteLine("");
+Console.WriteLine("You are " + Convert.ToString(age) + " years of age");
