b737eb1802b2223f28c9edee31cb1b434c6170c5
Mayorrkun <mayokunlaoshelawal@gmail.com>
2026-02-19 16:50:43 +0100

    Completed Exercise Array of Multiples

---
+++ ArrayofMultiples/ArrayofMultiples/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Define and initialize two integer num and length 
+ * create an int array with size length
+ * loop through and insert the (loop counter x num) into array
+ * output the final array
+ * 
+ */
+
+int num = 7, length = 12;
+
+int[] multiples = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+    multiples[i] = num * (i+1);
+}
+
+
+Console.WriteLine($"the number is {num} and the length is {length} \n ");
+
+Console.Write($"The current array: [ ");
+foreach (int i in multiples)
+{
+    Console.Write($" {i}, ");
+}
+Console.Write($"]");
